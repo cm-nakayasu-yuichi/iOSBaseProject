40b9d40ae77c92d1a11bed240bdc7e9b630501f4
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-04-17 11:47:24 +0900

    mutating を削除

--- BaseProject/Sources/Utilities/URLQueryItemEncoder.swift
+++ BaseProject/Sources/Utilities/URLQueryItemEncoder.swift
@@ -34,83 +34,83 @@ private class URLQueryItemEncoderKeyedContainer<Key: CodingKey>: KeyedEncodingCo
     
     var codingPath: [CodingKey]
     
-    mutating func encodeNil(forKey key: Key) throws {
+    func encodeNil(forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Bool, forKey key: Key) throws {
+    func encode(_ value: Bool, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: String, forKey key: Key) throws {
+    func encode(_ value: String, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Double, forKey key: Key) throws {
+    func encode(_ value: Double, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Float, forKey key: Key) throws {
+    func encode(_ value: Float, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Int, forKey key: Key) throws {
+    func encode(_ value: Int, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Int8, forKey key: Key) throws {
+    func encode(_ value: Int8, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Int16, forKey key: Key) throws {
+    func encode(_ value: Int16, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Int32, forKey key: Key) throws {
+    func encode(_ value: Int32, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: Int64, forKey key: Key) throws {
+    func encode(_ value: Int64, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: UInt, forKey key: Key) throws {
+    func encode(_ value: UInt, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: UInt8, forKey key: Key) throws {
+    func encode(_ value: UInt8, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: UInt16, forKey key: Key) throws {
+    func encode(_ value: UInt16, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: UInt32, forKey key: Key) throws {
+    func encode(_ value: UInt32, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode(_ value: UInt64, forKey key: Key) throws {
+    func encode(_ value: UInt64, forKey key: Key) throws {
         <#code#>
     }
     
-    mutating func encode<T>(_ value: T, forKey key: Key) throws where T : Encodable {
+    func encode<T>(_ value: T, forKey key: Key) throws where T : Encodable {
         <#code#>
     }
     
-    mutating func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type, forKey key: Key) -> KeyedEncodingContainer<NestedKey> where NestedKey : CodingKey {
+    func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type, forKey key: Key) -> KeyedEncodingContainer<NestedKey> where NestedKey : CodingKey {
         <#code#>
     }
     
-    mutating func nestedUnkeyedContainer(forKey key: Key) -> UnkeyedEncodingContainer {
+    func nestedUnkeyedContainer(forKey key: Key) -> UnkeyedEncodingContainer {
         <#code#>
     }
     
-    mutating func superEncoder() -> Encoder {
+    func superEncoder() -> Encoder {
         <#code#>
     }
     
-    mutating func superEncoder(forKey key: Key) -> Encoder {
+    func superEncoder(forKey key: Key) -> Encoder {
         <#code#>
     }
 }

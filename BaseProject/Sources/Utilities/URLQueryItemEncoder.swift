bfe2fa837d5f046b60c70d69195aeffceadcc3a2
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-04-17 23:13:45 +0900

    storeメソッド定義

--- BaseProject/Sources/Utilities/URLQueryItemEncoder.swift
+++ BaseProject/Sources/Utilities/URLQueryItemEncoder.swift
@@ -40,67 +40,72 @@ private class URLQueryItemEncoderKeyedContainer<Key: CodingKey>: KeyedEncodingCo
     init(encoder: URLQueryItemEncoder, codingPath: [CodingKey]) {
         self.encoder = encoder
         self.codingPath = codingPath
-        encoder.datastore.append([URLQueryItem]())
+    }
+    
+    private func store(value: Any, key: String) {
+        let describedValue = String(describing: value)
+        let queryItem = URLQueryItem(name: key, value: describedValue)
+        encoder.queryItems.append(queryItem)
     }
     
     func encodeNil(forKey key: Key) throws {
-        <#code#>
+        // NOP.
     }
     
     func encode(_ value: Bool, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: String, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Double, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Float, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Int, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Int8, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Int16, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Int32, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: Int64, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: UInt, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: UInt8, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: UInt16, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: UInt32, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode(_ value: UInt64, forKey key: Key) throws {
-        <#code#>
+        store(value: value, key: key.stringValue)
     }
     
     func encode<T>(_ value: T, forKey key: Key) throws where T : Encodable {
